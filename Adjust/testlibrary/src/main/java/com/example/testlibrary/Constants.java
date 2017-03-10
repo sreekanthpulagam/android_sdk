7d48aa8bbb5463009da9f44ed3c3beac2cb0f1e0
nonelse <pedro@adjust.com>
2017-03-10 21:24:51 +0100

    Update test library

---
+++ Adjust/testlibrary/src/main/java/com/example/testlibrary/Constants.java
@@ -0,0 +1,11 @@
+package com.example.testlibrary;
+
+/**
+ * Created by nonelse on 09.03.17.
+ */
+
+public interface Constants {
+    String LOGTAG = "TestLibrary";
+    String COOKIES_HEADER = "Set-Cookie";
+    String COOKIE_NAME = "session";
+}
---
+++ Adjust/testlibrary/src/main/java/com/example/testlibrary/ICommandJsonListener.java
@@ -0,0 +1,10 @@
+package com.example.testlibrary;
+
+/**
+ * Created by nonelse on 10.03.17.
+ */
+
+public interface ICommandJsonListener {
+    void executeCommand(String className, String methodName, String jsonParameters);
+
+}
