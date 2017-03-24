e4e692ae726cd4e94812f698af8b93c84ed80462
Obaied <abdullah.obaied@gmail.com>
2017-03-24 15:10:58 +0100

    adding extra commands and exception handling

--- Adjust/testapp/src/main/java/com/example/testapp/AdjustCommandExecutor.java
+++ Adjust/testapp/src/main/java/com/example/testapp/AdjustCommandExecutor.java
@@ -1,11 +1,10 @@
 package com.example.testapp;
 
 import android.content.Context;
+import android.net.*;
+import android.util.*;
 
-import com.adjust.sdk.Adjust;
-import com.adjust.sdk.AdjustConfig;
-import com.adjust.sdk.AdjustFactory;
-import com.adjust.sdk.LogLevel;
+import com.adjust.sdk.*;
 
 /**
  * Created by nonelse on 10.03.17.
@@ -14,6 +13,7 @@ import com.adjust.sdk.LogLevel;
 public class AdjustCommandExecutor extends ICommandExecutor {
     Context context;
     String basePath;
+    private static final String TAG = "AdjustCommandExecutor";
 
     public AdjustCommandExecutor(Context context) {
         this.context = context;
@@ -23,9 +23,28 @@ public class AdjustCommandExecutor extends ICommandExecutor {
     public void executeCommand(Command command) {
         super.executeCommand(command);
 
-        switch (command.methodName) {
-            case "onCreate": onCreate(); break;
-            case "teardown": teardown(); break;
+        try {
+            switch (command.methodName) {
+                case "onCreate": onCreate(); break;
+                case "trackEvent": trackEvent(); break;
+                case "onResume": onResume(); break;
+                case "onPause": onPause(); break;
+                case "setEnabled": setEnabled(); break;
+                case "setReferrer": setReferrer(); break;
+                case "setOfflineMode": setOfflineMode(); break;
+                case "sendFirstPackages": sendFirstPackages(); break;
+                case "addSessionCallbackParameter": addSessionCallbackParameter(); break;
+                case "addSessionPartnerParameter": addSessionPartnerParameter(); break;
+                case "removeSessionCallbackParameter": removeSessionCallbackParameter(); break;
+                case "removeSessionPartnerParameter": removeSessionPartnerParameter(); break;
+                case "resetSessionCallbackParameters": resetSessionCallbackParameters(); break;
+                case "resetSessionPartnerParameters": resetSessionPartnerParameters(); break;
+                case "setPushToken": setPushToken(); break;
+                case "teardown": teardown(); break;
+            }
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
+            Log.e(TAG, "executeCommand: failed to parse command. Check commands' syntax");
         }
     }
 
@@ -47,10 +66,104 @@ public class AdjustCommandExecutor extends ICommandExecutor {
         Adjust.onResume();
     }
 
-    private void teardown() {
+    private void trackEvent() throws NullPointerException {
+        String eventToken = command.getFirstParameterValue("eventToken");
+
+        AdjustEvent event = new AdjustEvent(eventToken);
+
+        String temp;
+        if((temp = command.getFirstParameterValue("currency")) != null) {
+            Double revenue = Double.valueOf(command.getFirstParameterValue("revenue"));
+            event.setRevenue(revenue, temp);
+        } else if((temp = command.getFirstParameterValue("callbackParams")) != null) {
+            String[] pairs = temp.split(",,,");
+            for (String pair : pairs) {
+                String[] keyAndValue = pair.split("@");
+                event.addCallbackParameter(keyAndValue[0], keyAndValue[1]);
+            }
+        } else if((temp = command.getFirstParameterValue("partnerParams")) != null) {
+            String[] pairs = temp.split(",,,");
+            for (String pair : pairs) {
+                String[] keyAndValue = pair.split("@");
+                event.addPartnerParameter(keyAndValue[0], keyAndValue[1]);
+            }
+        }
+
+        Adjust.trackEvent(event);
+    }
+
+    private void setReferrer() throws NullPointerException {
+        String referrer = command.getFirstParameterValue("referrer");
+        Adjust.setReferrer(referrer);
+    }
+
+    private void onPause() throws NullPointerException {
+        Adjust.onPause();
+    }
+
+    private void onResume() throws NullPointerException {
+        Adjust.onResume();
+    }
+
+    private void setEnabled() throws NullPointerException {
+        Boolean enabled = Boolean.valueOf(command.getFirstParameterValue("enabled"));
+        Adjust.setEnabled(enabled);
+    }
+
+    private void setOfflineMode() throws NullPointerException {
+        Boolean enabled = Boolean.valueOf(command.getFirstParameterValue("enabled"));
+        Adjust.setOfflineMode(enabled);
+    }
+
+    private void sendFirstPackages() throws NullPointerException {
+        Adjust.sendFirstPackages();
+    }
+
+    private void addSessionCallbackParameter() throws NullPointerException {
+        String key = command.getFirstParameterValue("key");
+        String value = command.getFirstParameterValue("value");
+
+        Adjust.addSessionCallbackParameter(key, value);
+    }
+
+    private void addSessionPartnerParameter() throws NullPointerException {
+        String key = command.getFirstParameterValue("key");
+        String value = command.getFirstParameterValue("value");
+
+        Adjust.addSessionPartnerParameter(key, value);
+    }
+
+    private void removeSessionCallbackParameter() throws NullPointerException {
+        String key = command.getFirstParameterValue("key");
+
+        Adjust.removeSessionCallbackParameter(key);
+    }
+
+    private void removeSessionPartnerParameter() throws NullPointerException {
+        String key = command.getFirstParameterValue("key");
+
+        Adjust.removeSessionCallbackParameter(key);
+    }
+
+    private void resetSessionCallbackParameters() throws NullPointerException {
+        Adjust.resetSessionCallbackParameters();
+    }
+
+    private void resetSessionPartnerParameters() throws NullPointerException {
+        Adjust.resetSessionPartnerParameters();
+    }
+
+    private void setPushToken() throws NullPointerException {
+        String token = command.getFirstParameterValue("token");
+
+        Adjust.setPushToken(token);
+    }
+
+    private void teardown() throws NullPointerException {
         String deleteStateString = command.getFirstParameterValue("deleteState");
         boolean deleteState = Boolean.parseBoolean(deleteStateString);
 
         AdjustFactory.teardown(deleteState);
     }
+
 }
