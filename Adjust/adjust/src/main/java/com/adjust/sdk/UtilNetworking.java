c4a78f5d637b8bb4420eb14a49de7acd49fb3546
nonelse <pedro@adjust.com>
2017-05-31 16:43:22 +0200

    Inject custom connection and test mode in sdk

--- Adjust/adjust/src/main/java/com/adjust/sdk/UtilNetworking.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/UtilNetworking.java
@@ -10,6 +10,7 @@ import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
@@ -39,11 +40,13 @@ public class UtilNetworking {
             URL url = new URL(urlString);
             HttpsURLConnection connection = AdjustFactory.getHttpsURLConnection(url);
             Map<String, String> parameters = new HashMap<String, String>(activityPackage.getParameters());
+            IConnectionOptions connectionOptions = AdjustFactory.getConnectionOptions();
 
             String appSecret = extractAppSecret(parameters);
 
             setDefaultHttpsUrlConnectionProperties(connection, activityPackage.getClientSdk());
             String authorizationHeader = buildAuthorizationHeader(parameters, appSecret, activityPackage.getClientSdk(), activityPackage.getActivityKind().toString());
+
             if (authorizationHeader != null) {
                 connection.setRequestProperty("Authorization", authorizationHeader);
             }
@@ -80,8 +83,11 @@ public class UtilNetworking {
             Uri uri = buildUri(activityPackage.getPath(), parameters, basePath);
             URL url = new URL(uri.toString());
             HttpsURLConnection connection = AdjustFactory.getHttpsURLConnection(url);
+            IConnectionOptions connectionOptions = AdjustFactory.getConnectionOptions();
 
             String authorizationHeader = buildAuthorizationHeader(parameters, appSecret, activityPackage.getClientSdk(), activityPackage.getActivityKind().toString());
+            connectionOptions.applyConnectionOptions(connection, activityPackage.getClientSdk());
+
             if (authorizationHeader != null) {
                 connection.setRequestProperty("Authorization", authorizationHeader);
             }
@@ -213,22 +219,13 @@ public class UtilNetworking {
         return result.toString();
     }
 
-    private static void setDefaultHttpsUrlConnectionProperties(HttpsURLConnection connection, String clientSdk) {
-        connection.setRequestProperty("Client-SDK", clientSdk);
-        connection.setConnectTimeout(Constants.ONE_MINUTE);
-        connection.setReadTimeout(Constants.ONE_MINUTE);
-
-        if (userAgent != null) {
-            connection.setRequestProperty("User-Agent", userAgent);
-        }
-    }
-
     private static Uri buildUri(String path, Map<String, String> parameters, String basePath) {
         Uri.Builder uriBuilder = new Uri.Builder();
 
         String scheme = Constants.SCHEME;
         String authority = Constants.AUTHORITY;
         String initialPath = "";
+
         try {
             String url = AdjustFactory.getBaseUrl();
             if (basePath != null) {
@@ -365,4 +362,20 @@ public class UtilNetworking {
         return null;
     }
 
+    public interface IConnectionOptions {
+        void applyConnectionOptions(HttpsURLConnection connection, String clientSdk);
+    }
+
+    static class ConnectionOptions implements IConnectionOptions {
+        @Override
+        public void applyConnectionOptions(HttpsURLConnection connection, String clientSdk) {
+            connection.setRequestProperty("Client-SDK", clientSdk);
+            connection.setConnectTimeout(Constants.ONE_MINUTE);
+            connection.setReadTimeout(Constants.ONE_MINUTE);
+
+            if (userAgent != null) {
+                connection.setRequestProperty("User-Agent", userAgent);
+            }
+        }
+    }
 }
